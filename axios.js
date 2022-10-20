e7293c4138f24e54c9216cb48b85701c0315c8f1
Jet Gunay <47313684+JetGunay@users.noreply.github.com>
2022-10-20 18:57:58 +0800

    Update axios.js

--- axios.js
+++ axios.js
@@ -4,7 +4,7 @@ const axiosClient = axios.create({
   baseURL: `https://www.wrike.com/api/v4/`,
   headers: {
     Authorization:
-      'Bearer eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjM4NDYzOTMsXCJpXCI6ODMzMTUyMixcImNcIjo0NjMyODc0LFwidVwiOjk1OTM4MTIsXCJyXCI6XCJVU1wiLFwic1wiOltcIldcIixcIkZcIixcIklcIixcIlVcIixcIktcIixcIkNcIixcIkRcIixcIk1cIixcIkFcIixcIkxcIixcIlBcIl0sXCJ6XCI6W10sXCJ0XCI6MH0iLCJpYXQiOjE2NjAyMTQ5NjR9.Cki7YlfsQrZH36ThVLzncKMWU9fzZhlgmNx6lQ9iZkQ',
+      'Bearer '+token,
   },
 });
 
